77e022bb6326b3e5409e1f480663a7f77c1ca4c9
Gabriel Paredes <gparedesferreira@gmail.com>
2023-04-15 04:37:15 -0300

    Update Dev.java
    Commit dev

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,19 +2,23 @@ package br.com.dio.desafio.dominio;
 
 import java.util.*;
 
-public class Dev {
+public class Dev 
+{
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){
+    public void inscreverBootcamp(Bootcamp bootcamp)
+    {
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
     }
 
-    public void progredir() {
+    public void progredir() 
+    {
         Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
-        if(conteudo.isPresent()) {
+        if(conteudo.isPresent()) 
+        {
             this.conteudosConcluidos.add(conteudo.get());
             this.conteudosInscritos.remove(conteudo.get());
         } else {
@@ -22,7 +26,8 @@ public class Dev {
         }
     }
 
-    public double calcularTotalXp() {
+    public double calcularTotalXp() 
+    {
         Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
         double soma = 0;
         while(iterator.hasNext()){
@@ -30,40 +35,42 @@ public class Dev {
             soma += next;
         }
         return soma;
-
-        /*return this.conteudosConcluidos
-                .stream()
-                .mapToDouble(Conteudo::calcularXp)
-                .sum();*/
     }
 
 
-    public String getNome() {
+    public String getNome() 
+    {
         return nome;
     }
 
-    public void setNome(String nome) {
+    public void setNome(String nome) 
+    {
         this.nome = nome;
     }
 
-    public Set<Conteudo> getConteudosInscritos() {
+    public Set<Conteudo> getConteudosInscritos() 
+    {
         return conteudosInscritos;
     }
 
-    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) 
+    {
         this.conteudosInscritos = conteudosInscritos;
     }
 
-    public Set<Conteudo> getConteudosConcluidos() {
+    public Set<Conteudo> getConteudosConcluidos() 
+    {
         return conteudosConcluidos;
     }
 
-    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) 
+    {
         this.conteudosConcluidos = conteudosConcluidos;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) 
+    {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
@@ -71,7 +78,8 @@ public class Dev {
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() 
+    {
         return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
     }
 }
